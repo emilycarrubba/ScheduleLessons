cef1f021c6afa566c4dec6a58461f3bccaebfc3f
APabloJ <altisimop@gmail.com>
2018-11-05 19:39:43 -0600

    added text to html on jumbotron and card titles.

--- project3/src/App.js
+++ project3/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+// import logo from "./logo.svg";
 import "./App.css";
 import { Button, Carousel } from "react-bootstrap";
 
@@ -45,16 +45,69 @@ class App extends Component {
           <div class="container">
             <p>Welcome to</p>
             <h1 class="display-4">ScheduleLessons</h1>
-            <p class="lead">
-              This is a modified jumbotron that occupies the entire horizontal
-              space of its parent.
-            </p>
+            <p class="lead">Scheduling made quick and simple.</p>
           </div>
         </div>
 
         <Button bsStyle="success">Success</Button>
+
         {/* Contextual button for informational alert messages */}
         {/*cards begin*/}
+
+        <div class="card" styles="width: 18rem;">
+          <img
+            class="card-img-top"
+            src=".../100px180/?text=Image cap"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Instructors</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+          </ul>
+          <div class="card-body">
+            <a href="#" class="card-link">
+              Card link
+            </a>
+            <a href="#" class="card-link">
+              Another link
+            </a>
+          </div>
+        </div>
+        <div class="card" styles="width: 18rem;">
+          <img
+            class="card-img-top"
+            src=".../100px180/?text=Image cap"
+            alt="Card image cap"
+          />
+          <div class="card-body">
+            <h5 class="card-title">Students</h5>
+            <p class="card-text">
+              Some quick example text to build on the card title and make up the
+              bulk of the card's content.
+            </p>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Cras justo odio</li>
+            <li class="list-group-item">Dapibus ac facilisis in</li>
+            <li class="list-group-item">Vestibulum at eros</li>
+          </ul>
+          <div class="card-body">
+            <a href="#" class="card-link">
+              Card link
+            </a>
+            <a href="#" class="card-link">
+              Another link
+            </a>
+          </div>
+        </div>
       </div>
     );
   }
